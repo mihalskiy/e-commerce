db4601de9f97c01afda90a799de969b0ca8c0ed2
mihalskiy <git config --global user.email>
2017-11-05 20:58:15 +0200

    added social block

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    sassLint = require('gulp-sass-lint');
 
 gulp.task('sass', function() {
     return gulp.src(['sass/**/*.sass', 'sass/**/*.scss'])
@@ -11,5 +12,12 @@ gulp.task('watch', function() {
     gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']);
 
 });
+
+gulp.task('default', function () {
+    return gulp.src('sass/**/*.s+(a|c)ss')
+      .pipe(sassLint())
+      .pipe(sassLint.format())
+      .pipe(sassLint.failOnError())
+  });
 
 gulp.task('default', ['watch']); 
