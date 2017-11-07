939d38dc97c60def77829ffe43c1cec41765823a
mihalskiy <git config --global user.email>
2017-11-07 16:18:01 +0200

    added responsive all browser

--- js/share.js
+++ js/share.js
@@ -1,4 +1,4 @@
-function my_f(objName) {
+function share(objName) {
     var object = document.getElementById(objName);
     object.style.display == 'block' ? object.style.display = 'none' : object.style.display = 'block'
  }
