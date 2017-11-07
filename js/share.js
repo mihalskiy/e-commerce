a4d30d08c01cb8db8d000eabe0815e5a0da93bef
mihalskiy <git config --global user.email>
2017-11-07 13:02:27 +0200

    added carusel

---
+++ js/share.js
@@ -0,0 +1,4 @@
+function my_f(objName) {
+    var object = document.getElementById(objName);
+    object.style.display == 'block' ? object.style.display = 'none' : object.style.display = 'block'
+ }
