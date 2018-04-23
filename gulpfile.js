e4f01bab46b528b924f2746967ca88e15f641262
mihalskiy <mihalskiy.rv@gmail.com>
2018-04-24 00:01:17 +0300

    settings gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
+    build = require('gulp-build'),
     sassLint = require('gulp-sass-lint');
+
+
+var options = {
+    /*partials: [{
+        name: 'footer',
+        tpl: '<p>Copyright 2013</p>'
+    }],*/
+    layout: './index.html'
+};
+
+gulp.task('build', function() {
+    gulp.src('./*.html')
+        .pipe(build({ title: 'Some page' }, options))
+        .pipe(gulp.dest('dist'))
+});
+
 
 gulp.task('sass', function() {
     return gulp.src(['assets/sass/style.sass', 'assets/sass/style.scss'])
